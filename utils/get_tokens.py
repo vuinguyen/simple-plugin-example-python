aafc30ffe5d665b893ebbc9c501aedfc524c55e2
Vui Nguyen <vui_engineer@yahoo.com>
2025-08-23 16:58:28 -0600

    JWT decoding

---
+++ utils/get_tokens.py
@@ -0,0 +1,38 @@
+import jwt
+import json
+import sys
+
+# Enter access token here
+# Enter id token here
+#try:
+ #   access_token = input("Enter access token: ").strip()
+ #   if not access_token:
+ #       print("Access token cannot be empty.")
+ #       exit(1)
+ #   id_token = input("Enter ID token: ").strip()
+ #   if not id_token:
+ #       print("ID token cannot be empty.")
+ #       exit(1)
+
+#if len(sys.argv) != 3:
+#    print("Usage: python get_tokens.py <access_token> <id_token>")
+#    sys.exit(1)
+
+access_token = sys.argv[1]
+id_token = sys.argv[2]
+
+try:
+    # Decode the access token
+    decoded_access_jwt = jwt.decode(access_token, options={"verify_signature": False})
+    print("Decoded Access Token:", json.dumps(decoded_access_jwt, indent=2))
+
+    # Decode the id token
+    decoded_id_jwt = jwt.decode(id_token, options={"verify_signature": False})
+    print("Decoded ID Token:", json.dumps(decoded_id_jwt, indent=2))
+except jwt.ExpiredSignatureError:
+    print("The token has expired.")
+except jwt.InvalidTokenError:
+    print("Invalid token. Please check your secret key and token.") 
+except Exception as e:
+    print(f"An error occurred: {e}")    
+
