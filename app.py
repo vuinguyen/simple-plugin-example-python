e60fd8299639e264ccb6c0fa335b6f98eb4b62e6
Vui Nguyen <vui_engineer@yahoo.com>
2025-08-23 18:37:16 -0600

    hardcode sample data to test displaying dynamic page

--- app.py
+++ app.py
@@ -9,3 +9,14 @@ def home():
 @app.route("/default")
 def default():
     return render_template("default.html")
+
+@app.route("/dynamic")
+def dynamic():
+    # if name and accounts count are not None, return a dynamic html page
+    # hardcoded for now, until we connect to the backend for real data
+    name = "Dynamic User"
+    accounts_count = 3
+    if name and accounts_count is not None:
+        return render_template("dynamic.html", name=name, accounts_count=accounts_count)
+    # else render default.html
+    return "hello, you're dynamic!"
